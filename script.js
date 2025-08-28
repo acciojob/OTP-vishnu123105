a0d85cfabbea799266b88e6d5165323329ffb28e
vishnuBandari <bandarivishnu143@gmail.com>
2025-08-28 17:42:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const inputs = document.querySelectorAll(".code");
+inputs[0].focus();
 
-    inputs[0].focus();
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
 
-    inputs.forEach((input, index) => {
-      input.addEventListener("input", (e) => {
-        const value = e.target.value;
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
 
-        if (value.length > 0 && index < inputs.length - 1) {
-          inputs[index + 1].focus();
-        }
-      });
-
-      input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace") {
-          if (input.value === "" && index > 0) {
-            inputs[index - 1].focus();
-            inputs[index - 1].value = "";
-          }
-        }
-      });
-    });
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value !== "") {
+        // If current field has a value, just clear it
+        input.value = "";
+      } else if (index > 0) {
+        // If empty, move back and clear previous
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      }
+      e.preventDefault(); // stop default cursor move
+    }
+  });
+});
