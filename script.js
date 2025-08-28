824f837613c5a24e3dab5895c0b00ceb4916b98e
vishnuBandari <bandarivishnu143@gmail.com>
2025-08-28 17:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+    inputs[0].focus();
+
+    inputs.forEach((input, index) => {
+      input.addEventListener("input", (e) => {
+        const value = e.target.value;
+
+        if (value.length > 0 && index < inputs.length - 1) {
+          inputs[index + 1].focus();
+        }
+      });
+
+      input.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+          if (input.value === "" && index > 0) {
+            inputs[index - 1].focus();
+            inputs[index - 1].value = "";
+          }
+        }
+      });
+    });
